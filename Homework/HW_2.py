3d6ff6a0a30dc664fd619e3f7046f0bfbf85f02f
elusiivee <andrii.1669@gmail.com>
2023-10-24 21:29:01 +0200

    adde second homework

--- Homework/HW_2.py
+++ Homework/HW_2.py
@@ -1,39 +1,97 @@
-class Client:
-    def __init__(self, name: str, order: int, discount):
-        self.name = name
-        self.order = order
-        self.discount = discount
-
-    def get_total_price(self):
-        if self.discount == 'Regular':
-            return RegularDiscount.get_discount(self.order)
-        elif self.discount == 'Silver':
-            return SilverDiscount.get_discount(self.order)
-        elif self.discount == 'Gold':
-            return GoldDiscount.get_discount(self.order)
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
-class RegularDiscount(Client):
+class LoggingMixin:
+    '''Mixin that adds logging to a class'''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = logging.getLogger(self.__class__.__name__)
 
-    @classmethod
-    def get_discount(cls, order):
-        return order - (order * 0.1)
-
-
-class SilverDiscount(Client):
-
-    @classmethod
-    def get_discount(cls, order):
-        return order - (order * 0.15)
-
-
-class GoldDiscount(Client):
-    @classmethod
-    def get_discount(cls, order):
-        return order - (order * 0.2)
+    def log(self, message, level=logging.INFO):
+        '''Log a message'''
+        self.logger.log(level, message)
+class Discount:
+    def discount(self):
+        raise NotImplementedError
 
 
-reg = Client('Lary', 57, 'Regular')
-sivl = Client('Jany', 33, 'Silver')
-gold = Client('Tom', 83, 'Gold')
-print(reg.get_total_price(), sivl.get_total_price(), gold.get_total_price())
+class RegularDiscount(Discount):
+    def discount(self):
+        return 0.9
+
+
+class SilverDiscount(Discount):
+    def discount(self):
+        return 0.85
+        # return 1.1
+
+
+class GoldDiscount(Discount):
+    def discount(self):
+        return 0.8
+
+
+class Product:
+    def __init__(self, title: str, price: float | int):
+        self.title = title
+        self.price = price
+
+    def __str__(self):
+        return f'{self.title}: {self.price:.2f}'
+
+
+class Cart(LoggingMixin):
+    def __init__(self, discount: Discount = None):
+        super().__init__()
+        self.__products = []
+        self.__quantities = []
+        try:
+            if discount is not None:
+                if not (0 <= discount.discount() <= 1):
+                    raise ValueError ('False discount')
+                else:
+                    self.discount = discount
+            else:
+                self.discount = None
+        except ValueError:
+            self.log(f'Invalid discount',logging.ERROR)
+            self.discount = None
+
+
+
+    def add_product(self, product: Product, quantity: int | float = 1):
+        if isinstance(product, Product) and isinstance(quantity, int | float):
+            self.log('add product to cart')
+            self.__products.append(product)
+            self.__quantities.append(quantity)
+        else:
+            self.log(f'inpossible to add {product}', logging.WARNING)
+    def total(self):
+        summa = sum(product.price * quantity for product, quantity in zip(self.__products, self.__quantities))
+        summa = summa * self.discount.discount() if self.discount else summa
+        self.log(f'Customer pay {summa}', logging.WARNING)
+        return summa
+    def __str__(self):
+        return '\n'.join(map(lambda items: f'{items[0]} x {items[1]} = {items[0].price * items[1]} UAH',
+                          zip(self.__products, self.__quantities))) + f'\nTotal: {self.total():.2f} UAH\n'
+
+
+pr_1 = Product('banana', 50)
+pr_2 = Product('apple', 51)
+pr_3 = Product('orange', 52)
+
+
+
+cart_1 = Cart()
+cart_2 = Cart(SilverDiscount())
+
+cart_1.add_product(pr_1)
+cart_1.add_product(pr_2)
+cart_1.add_product(pr_3)
+
+
+cart_2.add_product(pr_1, 5)
+
+print(cart_1)
+print(cart_2)
