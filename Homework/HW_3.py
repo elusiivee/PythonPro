c4bbbf8ebf648b3d9a95c1cf6cc7cf5e04f791a0
elusiivee <andrii.1669@gmail.com>
2023-11-08 16:21:46 +0100

    added HW_3

--- Homework/HW_3.py
+++ Homework/HW_3.py
@@ -1,22 +1,51 @@
-#1
-def generator(start,amount, rule):
-    i=0
-    while i< amount:
+# 1
+def generator(start, amount, rule):
+    i = 0
+    while i < amount:
         yield start
         start = rule(start)
-        i+=1
+        i += 1
+
 
 def rule(n):
-    return n*2
+    return n * 2
     # return n**2               #чому квадрат не працює
 
-funk=generator(1,10,rule)
+
+funk = generator(1, 10, rule)
 
 for element in funk:
     print(element)
 
-#2
+# 2
+
+# def fibonachi_gen(lim: int):
+#     first, second = 1, 1
+#     while first <= lim:
+#         yield first
+#         first, second = second, first+second
+#
+#
+# print('\nFibonachi:')
+# for item in fibonachi_gen(144):
+#     print(item)
+
+
+# 3
+new_list = []
+
+
+def list_gen(list,funk):
+    return sum(funk(list))
 
 
 
+print('\nList:')
 
+
+def extra_funk(l):
+    for i in l:
+        new_list.append(i * 10)
+    return new_list
+
+print(list_gen([1,2,3,4,5,6],extra_funk))
