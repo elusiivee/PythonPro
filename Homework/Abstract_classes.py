215d1db178ca1ca915be734c201815b7f6590d30
elusiivee <andrii.1669@gmail.com>
2023-11-17 17:48:07 +0100

    updated Abstract_classes, added class Circle and param size

--- Homework/Abstract_classes.py
+++ Homework/Abstract_classes.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Figure:
 
     def perimeter(self):
@@ -11,30 +12,56 @@ class Figure:
 
 class Triangle(Figure):
 
-    def __init__(self, a_side, b_side, c_side):
+    def __init__(self, a_side, b_side, c_side, size):
         """
         :param a_side: a side of triangle -> int|float
         :param b_side: b side of triangle -> int|float
         :param c_side: c side of triangle -> int|float
+        :param size: size of triangle -> str
         """
-        if isinstance(a_side,int|float) and isinstance(b_side,int|float) and isinstance(c_side,int|float):
+        if isinstance(a_side, int | float) and isinstance(b_side, int | float) and isinstance(c_side,
+                                                                                              int | float) and isinstance(
+                size, str):
             self.a_side = a_side
             self.b_side = b_side
             self.c_side = c_side
+            self.size = size
         else:
             raise ValueError('You should input numbers')
 
     def perimeter(self):
-        return f'Perimeter of triangel: {self.a_side + self.b_side + self.c_side}'
+        return f'Perimeter of triangel: {self.a_side + self.b_side + self.c_side} {self.size}'
 
     def area(self):
-        p=(self.a_side + self.b_side + self.c_side)/2
-        return f'Perimeter of triangel: {round(math.sqrt(p * (p - self.a_side) * (p - self.b_side) * (p - self.c_side)),2)}'
+        p = (self.a_side + self.b_side + self.c_side) / 2
+        return f'Area of triangle: {round(math.sqrt(p * (p - self.a_side) * (p - self.b_side) * (p - self.c_side)), 2)} {self.size}^2'
+
+
+class Circle(Figure):
+
+    def __init__(self, radius, size):
+        """
+        :param radius: a side of triangle -> int|float
+        :param size: size of triangle -> str
+        """
+        if isinstance(radius, int | float):
+            self.radius = radius
+            self.size = size
+        else:
+            raise ValueError('You should input numbers')
+
+    def perimeter(self):
+        return f'Perimeter of circle: {round(2 * self.radius * math.pi, 2)} {self.size}'
+
+    def area(self):
+        return f'Area of circle: {round(self.radius * math.pi ** 2, 2)} {self.size}^2'
 
 
 def figure(obj):
     return obj
 
 
-print(figure(Triangle(2,3,4)).perimeter())
-print(figure(Triangle(2,3,4)).area())
+print(figure(Triangle(2, 3, 4, 'cm')).perimeter())
+print(figure(Triangle(2, 3, 4, 'm')).area())
+print(figure(Circle(5,'m')).perimeter())
+print(figure(Circle(5,'cm')).area())
